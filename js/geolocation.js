7b3b1770c3e1bf65875aaa38774d47154b975b35
AlexKryvets <alex.kryvets@gmail.com>
2013-02-28 12:41:26 +0200

    defined abstract provider; implements singleton geolocation source (provider); refactoring of existing code;
    * Resolved problem with  the serial coordinates *

--- js/geolocation.js
+++ js/geolocation.js
@@ -3,12 +3,11 @@
 	window.geolocation.getCurrentPosition = function () {		
 		var onSuccess = (typeof arguments[0] === 'function') ? arguments[0] : function () {},
 			onError = (typeof arguments[1] === 'function') ? arguments[1] : defaultErrorHandler,
-			options = (typeof arguments[1] !== 'function') ? (arguments[1] || {}) : (arguments[2] || {}),
-			identifier = null;
+			options = (typeof arguments[1] !== 'function') ? (arguments[1] || {}) : (arguments[2] || {});
 		options.maxAccuracy = options.maxAccuracy || 100;
 		if (geolocation) {
 			if (options.enableHighAccuracy) {
-				identifier = getHighAccuracyPosition(
+				getHighAccuracyPosition(
 					function (highPosition) {
 						onSuccess(highPosition, window.geolocation.MAX_ACCURACY);
 					},
@@ -30,46 +29,35 @@
 		} else {
 			onError({UNKNOWN_ERROR: 0, PERMISSION_DENIED: 1, POSITION_UNAVAILABLE: 2, TIMEOUT: 3, code: 4});
 		}
-		return identifier;
 	};	
 	window.geolocation.clearWatch = function (identifier) {	
-		if (identifier != null) {
-			geolocation.clearWatch(identifier.watchId);
-			clearTimeout(identifier.setTimeoutId);
-		}
 	};	
 	var getLowAccuracyPosition = function (onSuccess, onError, options) {
-		geolocation.getCurrentPosition(
-			function (position) {
-				onSuccess(position); 
-			},
-			onError, options
-		);
+		geolocation.getCurrentPosition(onSuccess, onError, options);
 	};	
 	var getHighAccuracyPosition = function (onSuccess, onError, options) {
-		var lowPosition = null;
-		var watchId = geolocation.watchPosition(
-			function (position) {
+		var provider = new SingletonGeolocationProvider(options);
+		var listener = {
+			onSuccess: function (position) {
 				if (position.coords.accuracy < options.maxAccuracy) {
-					geolocation.clearWatch(watchId);
 					clearTimeout(setTimeoutId);
-					onSuccess(position); 
+					onSuccess(position);
+					provider.removeListener(listener);
 				}
-				lowPosition = position;
 			}, 
-			function (error) {
+			onError: function (error) {
 				clearTimeout(setTimeoutId);
-				geolocation.clearWatch(watchId);
-				onError(error, lowPosition);
-			}, options
-		);
+				onError(error, provider.getPosition());
+				provider.removeListener(listener);
+			}
+		};
 		var setTimeoutId = setTimeout(
 			function () { 
-				geolocation.clearWatch(watchId); 
-				onError({UNKNOWN_ERROR: 0, PERMISSION_DENIED: 1, POSITION_UNAVAILABLE: 2, TIMEOUT: 3, code: 3}, lowPosition); 
+				onError({UNKNOWN_ERROR: 0, PERMISSION_DENIED: 1, POSITION_UNAVAILABLE: 2, TIMEOUT: 3, code: 3}, provider.getPosition()); 
+				provider.removeListener(listener);
 			}, options.timeout + 1000
 		);
-		return {watchId: watchId, setTimeoutId: setTimeoutId};
+		provider.addListener(listener);
 	};	
 	var defaultErrorHandler = function (error) {
 		switch (error.code)	{
@@ -90,4 +78,84 @@
 				break;
 		}	
 	};	
+	var extend = (function () {
+		var F = function () {};
+		return function (C, P) {
+			F.prototype = P.prototype;
+			C.prototype = new F();
+			C.parent = P.prototype;
+			C.prototype.constructor = C;
+		}
+	}());
+	var AbstractProvider = function AbstractProvider () {};
+	AbstractProvider.prototype.addListener = function (listener) {
+		for (i in this._listeners) {
+			if (listener == this._listeners[i]) {
+				return;
+			}
+		}
+		listener.onSuccess = listener.onSuccess || function () {};
+		listener.onError = listener.onError || function () {};
+		this._listeners.push(listener);
+	};
+	AbstractProvider.prototype.removeListener = function (listener) {
+		for (i in this._listeners) {
+			if (listener == this._listeners[i]) {
+				this._listeners.splice(i, 1);
+				break;
+			}
+		}
+	};
+	AbstractProvider.prototype.notifySuccess = function (position) {
+		for (i in this._listeners) {
+			this._listeners[i].onSuccess(position);
+		}
+	};
+	AbstractProvider.prototype.notifyError = function (error) {
+		for (i in this._listeners) {
+			this._listeners[i].onError(error);
+		}
+	};
+	var SingletonGeolocationProvider;
+	(function () {
+		var instance, options, position = null, watchId = null;
+		SingletonGeolocationProvider = function SingletonGeolocationProvider (o) {
+			options = o;
+			if (instance) {
+				return instance;
+			}
+			instance = this;
+			this._listeners = [];
+			this.addListener = function (listener) {
+				SingletonGeolocationProvider.prototype.addListener.apply(instance, arguments);
+				if (watchId == null) {
+					watchId = geolocation.watchPosition(
+						function (p) { 
+							instance.notifySuccess(p); 
+							position = p;
+						}, 
+						function (e) { 
+							instance.notifyError(e); 
+						},
+						options
+					);
+				}
+			};
+			this.removeListener = function (listener) {
+				SingletonGeolocationProvider.prototype.removeListener.apply(instance, arguments);
+				if (this._listeners.length == 0) {
+					geolocation.clearWatch(watchId);
+					watchId = null;
+					position = null;
+				}
+			};
+			this.getWatchId = function () {
+				return watchId;
+			};
+			this.getPosition = function () {
+				return position;
+			};
+		};
+		extend(SingletonGeolocationProvider, AbstractProvider);
+	}());
 }(navigator.geolocation));
